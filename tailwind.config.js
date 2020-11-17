346447e1f1e93e65247102b74714f1f3d356ae03
simonhernandez <48769693+simonhernandez@users.noreply.github.com>
2020-11-17 04:01:47 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,55 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+
+    maxWidth: {
+      xs: '21.875rem',
+    },
+
+    extend: {
+      colors: {
+        primary: '#19A1AE',
+        grey: '#6B7082',
+        dark: '#2E3349',
+        lightGrey: '#E8E9EC'
+      },
+      fontFamily: {
+        body: ['"Kumbh Sans"']
+      },
+      fontSize: {
+        xxs: '.625rem',
+      },
+      inset: {
+        '-10%': '-10%',
+        '-15%': '-15%',
+        '-20%': '-20%',
+        '-25%': '-25%',
+        '-30%': '-30%',
+        '-35%': '-35%',
+        '-45%': '-45%',
+        '-55%': '-55%',
+        '-65%': '-65%',
+        '-75%': '-75%',
+        '-80%': '-80%',
+        '-85%': '-85%',
+        '-88%': '-88%',
+        '-90%': '-90%',
+        '-95%': '-95%',
+      },
+      width: {
+        '622': '622px',
+        '978': '978px',
+      },
+      height: {
+        '621': '621px',
+        '976': '976px',
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
